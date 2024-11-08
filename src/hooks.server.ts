268558a95741102a443694d6c0ba7c1e31557f68
dene- <ekisde.xp@gmail.com>
2024-11-08 19:29:39 +0100

    chore(app): security improvements 4 - headers

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -4,6 +4,9 @@ const securityHeaders = {
   'Cross-Origin-Embedder-Policy': 'require-corp',
   'Cross-Origin-Opener-Policy': 'same-origin',
   'X-XSS-Protection': '0',
+  'X-Content-Type-Options': 'nosniff',
+  'Referrer-Policy': 'no-referrer',
+  'Permissions-Policy': 'interest-cohort=()',
 };
 
 export const handle: Handle = async ({ event, resolve }) => {
